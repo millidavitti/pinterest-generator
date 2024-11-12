612c63c63a72ad1d9507316091f611807b980099
davitti <abuadonald@gmail.com>
2024-11-12 23:38:01 +0100

    Create `InteractiveIcon` component with click handler and configurable classes

---
+++ src/components/layouts/interactive_icon.tsx
@@ -0,0 +1,22 @@
+import { MouseEventHandler, ReactNode } from "react";
+import { cn } from "../../utils/cn";
+
+interface InteractiveIcon {
+	children: ReactNode;
+	className?: String;
+	callback?: MouseEventHandler<HTMLDivElement> | undefined;
+}
+export default function InteractiveIcon({
+	children,
+	className,
+	callback,
+}: InteractiveIcon) {
+	return (
+		<div
+			className={cn("p-3 cursor-pointer active:scale-95 transition", className)}
+			onClick={callback}
+		>
+			{children}
+		</div>
+	);
+}
