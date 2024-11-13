1744cff3e9b214e8ecb6cc4aa57dafa97a89cc31
davitti <abuadonald@gmail.com>
2024-11-13 07:53:01 +0100

    Add image preview with dynamic URLs from `image_url_jotai` in `pin-preview.tsx`

--- src/app/(home)/components/pin-preview.tsx
+++ src/app/(home)/components/pin-preview.tsx
@@ -1,7 +1,12 @@
+"use client";
 import FlexColumn from "@/components/layouts/flex_col";
 import FlexRow from "@/components/layouts/flex_row";
+import { image_url_jotai } from "@/data/app";
+import { useAtomValue } from "jotai";
+import Image from "next/image";
 
 export default function PinPreview() {
+	const image_url = useAtomValue(image_url_jotai);
 	return (
 		<FlexColumn className='h-full w-full p-3 gap-3'>
 			{/* Header */}
@@ -12,8 +17,22 @@ export default function PinPreview() {
 			<FlexColumn className='h-full rounded-none'>
 				{/* Pin */}
 				<FlexColumn className='outline h-full rounded-none'>
-					<FlexRow className='h-full rounded-none border-b-4 border-white bg-light-surface'></FlexRow>
-					<FlexRow className='h-full rounded-none bg-light-surface'></FlexRow>
+					<FlexRow className='h-full rounded-none border-b-4 border-white bg-light-surface'>
+						<Image
+							alt=''
+							src={image_url.top! || "/file.svg"}
+							width={750}
+							height={1000}
+						/>
+					</FlexRow>
+					<FlexRow className='h-full rounded-none bg-light-surface'>
+						<Image
+							alt=''
+							src={image_url.bottom! || "/file.svg"}
+							width={750}
+							height={1000}
+						/>
+					</FlexRow>
 				</FlexColumn>
 			</FlexColumn>
 		</FlexColumn>
