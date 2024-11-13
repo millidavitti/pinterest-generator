a89e6d9f5ded480f0f44e0127ab076ce55afd9d1
davitti <abuadonald@gmail.com>
2024-11-13 01:17:24 +0100

    Create `Header` component with title and privacy policy link in `FlexRow` layout

---
+++ src/app/(home)/components/header.tsx
@@ -0,0 +1,10 @@
+import FlexRow from "@/components/layouts/flex_row";
+
+export default function Header() {
+	return (
+		<FlexRow className='justify-between p-6'>
+			<h1>PinWizard</h1>
+			<p>Privacy Policy</p>
+		</FlexRow>
+	);
+}
