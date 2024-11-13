b1cc276d57adc47d7693108f1863d88e598108f7
davitti <abuadonald@gmail.com>
2024-11-13 03:26:56 +0100

    Create `TopImage` component with `CaptureImage` in `top-image.tsx`

---
+++ src/app/(home)/components/top-image.tsx
@@ -0,0 +1,11 @@
+import FlexColumn from "@/components/layouts/flex_col";
+import CaptureImage from "./capture-image";
+
+export default function TopImage() {
+	return (
+		<FlexColumn className='p-3 h-full gap-3'>
+			<h2 className='text-lg'>Top Image</h2>
+			<CaptureImage />
+		</FlexColumn>
+	);
+}
