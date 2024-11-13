d4b01f0926a53f0e9f11d099def6487ed7eec891
davitti <abuadonald@gmail.com>
2024-11-13 22:28:46 +0100

    Add `TemplateHeader` and `Templates` components to `PinGenerator` layout

--- src/app/(home)/components/pin-generator.tsx
+++ src/app/(home)/components/pin-generator.tsx
@@ -3,13 +3,22 @@ import PinGeneratorHeader from "./pin-generator-header";
 import FlexRow from "@/components/layouts/flex_row";
 import UploadedImages from "./uploaded-images";
 import PinPreview from "./pin-preview";
+import TemplateHeader from "./template-header";
+import Templates from "./templates";
 
 export default function PinGenerator() {
 	return (
 		<FlexColumn className='w-full p-3 bg-transparent gap-3'>
 			<PinGeneratorHeader />
 			<FlexRow className='gap-3 bg-transparent h-full'>
-				<UploadedImages />
+				<FlexColumn className='w-full h-full'>
+					<UploadedImages />
+					<FlexColumn className='shrink-0'>
+						<TemplateHeader />
+						{/* Templates */}
+						<Templates />
+					</FlexColumn>
+				</FlexColumn>
 				<PinPreview />
 			</FlexRow>
 		</FlexColumn>
