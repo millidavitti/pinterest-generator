89ee689a233a416111da2afb49a3c3543ed08782
davitti <abuadonald@gmail.com>
2024-11-13 01:18:49 +0100

    Create `SideBar` component with `SchedulePost`, `AddTopic`, `SearchTopic`, and `Topics` in a `FlexColumn` layout

---
+++ src/app/(home)/components/side-bar.tsx
@@ -0,0 +1,18 @@
+import FlexColumn from "@/components/layouts/flex_col";
+import SchedulePost from "./schedule-post";
+import AddTopic from "./add-topic";
+import SearchTopic from "./search-topic";
+import Topics from "./topics";
+
+export default function SideBar() {
+	return (
+		<FlexColumn className='w-[500px] gap-3 bg-light-surface-surface-container-lowest'>
+			<FlexColumn className='gap-3 p-3'>
+				<SchedulePost />
+				<AddTopic />
+				<SearchTopic />
+				<Topics />
+			</FlexColumn>
+		</FlexColumn>
+	);
+}
