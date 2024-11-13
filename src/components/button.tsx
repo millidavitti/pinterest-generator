c91e4e1520ac40dfd651bd6658595affad2f3e71
davitti <abuadonald@gmail.com>
2024-11-13 03:23:36 +0100

    Add `font-semibold` to button styling in `button.tsx`

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,7 +9,7 @@ export default function Button({ children, className }: Button) {
 		<button
 			type='button'
 			className={cn(
-				"flex w-fit bg-light-primary p-3 rounded-lg text items-center gap-2 text-light-primary-on-primary cursor-pointer active:scale-[.99] transition-transform",
+				"flex w-fit bg-light-primary p-3 py-2 rounded-lg text items-center gap-2 text-light-primary-on-primary cursor-pointer active:scale-[.99] transition-transform font-semibold",
 				className,
 			)}
 		>
