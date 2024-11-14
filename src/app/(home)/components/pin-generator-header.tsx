c9a060b83609f94d971f7cda7a5139ebcf5edbdb
davitti <abuadonald@gmail.com>
2024-11-14 09:15:06 +0100

    Integrate image download functionality with `dom-to-image-more` and dynamic file naming

--- src/app/(home)/components/pin-generator-header.tsx
+++ src/app/(home)/components/pin-generator-header.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Button from "@/components/button";
 import FlexColumn from "@/components/layouts/flex_col";
 import FlexRow from "@/components/layouts/flex_row";
 import { Download } from "lucide-react";
-
+import domtoimage from "dom-to-image-more";
+import { useAtomValue } from "jotai";
+import { selected_topic_jotai } from "@/data/app";
+let id = 1;
 export default function PinGeneratorHeader() {
+	const selected_topic = useAtomValue(selected_topic_jotai);
 	return (
 		<FlexRow className='justify-between items-center bg-transparent h-fit shrink-0'>
 			<FlexColumn className='bg-transparent'>
@@ -12,10 +17,40 @@ export default function PinGeneratorHeader() {
 					Create beautiful Pins in seconds
 				</p>
 			</FlexColumn>
-			<Button>
-				<Download />
-				<span>Download Pin</span>
-			</Button>
+			<a id='download'>
+				<Button
+					onClick={() => {
+						const node = document.getElementById("pin")!;
+						const download = document.getElementById(
+							"download",
+						)! as HTMLAnchorElement;
+						node.style.display = "flex";
+
+						domtoimage
+							.toPng(node, { adjustClonedNode: adjustClone })
+							.then(function (dataUrl: string) {
+								download.href = dataUrl;
+								download.download =
+									selected_topic.replaceAll(" ", "_").toLowerCase() +
+									id +
+									".png";
+								id++;
+							});
+					}}
+				>
+					<Download />
+					<span>Download Pin</span>
+				</Button>
+			</a>
 		</FlexRow>
 	);
+}
+function adjustClone(node: HTMLElement, clone: HTMLElement, after: boolean) {
+	if (after && clone.id === "text-box-container") {
+		clone.style.transform = "scale(2.3)";
+	}
+	if (!after && clone.id === "cta") {
+		clone.style.transform = "scale(2)";
+	}
+	return clone;
 }
