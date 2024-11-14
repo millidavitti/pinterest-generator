2913c83dd9323c23fdf46b6336c72426bce6d513
davitti <abuadonald@gmail.com>
2024-11-14 04:43:32 +0100

    Style `CTA` component with dynamic `template_color` and fixed positioning

---
+++ src/app/(home)/components/cta.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Poppins } from "next/font/google";
+import FlexRow from "@/components/layouts/flex_row";
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/utils/cn";
+import { useAtomValue } from "jotai";
+import { template_color_jotai } from "@/data/app";
+
+export default function CTA() {
+	const template_color = useAtomValue(template_color_jotai);
+	return (
+		<FlexRow
+			className={cn(
+				"font-bold w-fit text-white shrink-0 absolute bottom-6 inset-x-0 mx-auto p-3 rounded-none ",
+				poppins.className,
+				"bg-[" + template_color + "]",
+			)}
+		>
+			START QUIZ <ChevronRight stroke='white' />
+		</FlexRow>
+	);
+}
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["500"],
+});
