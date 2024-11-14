063d9b92bf307f8d9e299b3c301b37bf792620d5
davitti <abuadonald@gmail.com>
2024-11-14 04:40:58 +0100

    "Add Jotai atoms for `template_color`, `selected_template`, and `text_box_default_font`, and define `QuizData` type"

--- src/data/app.ts
+++ src/data/app.ts
@@ -12,4 +12,16 @@ export const image_url_jotai = atom<ImageURL>({ top: null, bottom: null });
 
 export const topics_jotai = atom<string[]>([]);
 
-export const selected_topic_jotai = atom<string>("<YOUR TOPIC>");
+export const selected_topic_jotai = atom<string>("<YOUR TOPIC> ");
+
+export const template_color_jotai = atom<string>("#5e17eb");
+
+export type QuizData = (
+	| { white: string }
+	| { green: string }
+	| { highlight: string }
+)[];
+
+export const selected_template_jotai = atom<QuizData>();
+
+export const text_box_default_font_jotai = atom<number>(25);
