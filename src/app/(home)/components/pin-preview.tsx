9195b98e5a5305dd6b10c79b6aebc067ebd6c5ba
davitti <abuadonald@gmail.com>
2024-11-14 09:15:37 +0100

    Refactor `PinPreview` to use the `Pin` component

--- src/app/(home)/components/pin-preview.tsx
+++ src/app/(home)/components/pin-preview.tsx
@@ -1,13 +1,9 @@
 "use client";
 import FlexColumn from "@/components/layouts/flex_col";
 import FlexRow from "@/components/layouts/flex_row";
-import { image_url_jotai } from "@/data/app";
-import { useAtomValue } from "jotai";
-import TopPreview from "./top-preview";
-import BottomPreview from "./bottom-preview";
+import Pin from "./pin";
 
 export default function PinPreview() {
-	const image_url = useAtomValue(image_url_jotai);
 	return (
 		<FlexColumn className='h-full w-[750px] p-3 gap-3'>
 			{/* Header */}
@@ -16,11 +12,7 @@ export default function PinPreview() {
 			</FlexRow>
 			{/* Preview */}
 			<FlexColumn className='h-full rounded-none'>
-				{/* Pin */}
-				<FlexColumn className='outline h-full rounded-none'>
-					<TopPreview url={image_url.top!} />
-					<BottomPreview url={image_url.bottom!} />
-				</FlexColumn>
+				<Pin />
 			</FlexColumn>
 		</FlexColumn>
 	);
