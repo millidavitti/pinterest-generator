652204653d10ed89aa826264537f97d9ff294ba9
davitti <abuadonald@gmail.com>
2024-11-14 04:40:29 +0100

    "Update `Topics` component to use `uuid` for unique keys instead of array index"

--- src/app/(home)/components/topics.tsx
+++ src/app/(home)/components/topics.tsx
@@ -3,13 +3,13 @@ import FlexColumn from "@/components/layouts/flex_col";
 import { topics_jotai } from "@/data/app";
 import { useAtomValue } from "jotai";
 import Topic from "./topic";
-
+import { v4 as uuidv4 } from "uuid";
 export default function Topics() {
 	const topics = useAtomValue(topics_jotai);
 	return (
 		<FlexColumn className='gap-3'>
 			{topics.map((topic, i) => (
-				<Topic key={i} topic={topic} />
+				<Topic key={uuidv4()} topic={topic} />
 			))}
 		</FlexColumn>
 	);
