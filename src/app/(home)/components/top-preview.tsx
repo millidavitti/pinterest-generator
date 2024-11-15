7453d49093254a58d2208659048af88c0d3ebdbc
davitti <abuadonald@gmail.com>
2024-11-15 12:52:59 +0100

    Simplify `TopPreview` component by removing unused references and effects.

--- src/app/(home)/components/top-preview.tsx
+++ src/app/(home)/components/top-preview.tsx
@@ -1,32 +1,11 @@
 import FlexRow from "@/components/layouts/flex_row";
-import { checkOverflow } from "@/utils/check-overflow";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
 import TextBox from "./text-box";
-import { text_box_default_font_jotai } from "@/data/app";
-import { useAtom } from "jotai";
-import { getLineCount } from "@/utils/get-line-count";
 
 interface TopPreview {
 	url: string;
 }
 export default function TopPreview({ url }: TopPreview) {
-	const textBoxRef = useRef<HTMLDivElement>(null);
-	const [text_box_default_font, text_box_default_font_setter] = useAtom(
-		text_box_default_font_jotai,
-	);
-
-	useEffect(() => {
-		if (textBoxRef.current) {
-			if (getLineCount(textBoxRef.current) > 2)
-				checkOverflow(
-					textBoxRef.current,
-					text_box_default_font_setter,
-					text_box_default_font,
-				);
-		}
-	}, [text_box_default_font, text_box_default_font_setter]);
-
 	return (
 		<>
 			<FlexRow className='relative h-full rounded-none bg-light-surface overflow-visible'>
@@ -48,7 +27,7 @@ export default function TopPreview({ url }: TopPreview) {
 					/>
 				)}
 				{/* Text Box */}
-				<TextBox textBoxRef={textBoxRef} />
+				<TextBox />
 			</FlexRow>
 		</>
 	);
